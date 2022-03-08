d7bb42dc3340e0a1c52dcce6ecbead1fe50cd789
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-08 00:31:41 -0500

    add date picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,12 +136,21 @@ $(".list-group").on("click", "span", function() {
   //swap out elements 
   $(this).replaceWith(dateInput); 
 
+  // enable jquery ui date picker 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element 
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
   .val()
@@ -225,6 +234,7 @@ $(".card .list-group").sortable({
   }
 })
 
+// delete an individual task 
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
@@ -239,6 +249,12 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+// date picker 
+$("#modalDueDate").datepicker( {
+  minDate: 1
+});
+
 
 // load tasks for the first time
 loadTasks();
